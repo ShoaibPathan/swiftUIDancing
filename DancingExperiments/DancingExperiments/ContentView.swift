b230b12870cf034359ead15545c7264589cc8a51
Romy Ilano <romy.ilano@gmail.com>
2020-06-20 12:39:21 -0700

    Made a new SwiftUI Project

---
+++ DancingExperiments/DancingExperiments/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DancingExperiments
+//
+//  Created by Romy Ilano on 6/20/20.
+//  Copyright © 2020 Romy IlanoTest. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
